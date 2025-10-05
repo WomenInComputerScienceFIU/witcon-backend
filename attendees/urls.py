cb01a0c1407a69c36093fb2857e3750598a57e89
Shatoya <shatoyagg.6@gmail.com>
2025-10-04 21:11:42 -0400

    Add admin url

--- attendees/urls.py
+++ attendees/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from attendees.views import AttendeeViewSet
@@ -6,6 +7,8 @@ router = DefaultRouter()
 router.register(r'attendees', AttendeeViewSet, basename='attendee')
 
 urlpatterns = [
-    path("api/", include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),  # <-- router is under /api/
 ]
+
 
