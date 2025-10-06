794c4778e8c8487ca230a8d2ce35ddd463852cea
Shatoya <shatoyagg.6@gmail.com>
2025-10-06 10:34:49 -0400

    update

--- backend/urls.py
+++ backend/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("backend-api/", include("attendees.urls")), 
+    path("backend-api", include("attendees.urls")), 
 ]
 
 if settings.DEBUG:
